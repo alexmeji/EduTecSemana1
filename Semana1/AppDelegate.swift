cfca9c70e71b203ac9ba31f3a44178a116cc9c01
Alex Mejicanos <alexmejicanos@outlook.com>
2017-03-08 12:43:45 -0600

    Paso 2 ( Ciclo de vida del Controlador)

--- Semana1/AppDelegate.swift
+++ Semana1/AppDelegate.swift
@@ -16,35 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("Termino cargar con Opciones")
+        //print("Termino cargar con Opciones")
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        print("Dejo de estar Activa")
+        //print("Dejo de estar Activa")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        print("La aplicación entro a Background")
+        //print("La aplicación entro a Background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print("La aplicación regreso de Background")
+        //print("La aplicación regreso de Background")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print("La aplicación paso a estar activa")
+        //print("La aplicación paso a estar activa")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        print("Se termino la aplicación")
+        //print("Se termino la aplicación")
     }
 
 
